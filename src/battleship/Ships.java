23331e332fefd17594eab5969486608aaa8015ae
Alexis LaparraPerez-150003278 <150003278@vusdnoc.vusd.k12.ca.us>
2018-11-07 10:38:51 -0800

    s

--- src/battleship/Ships.java
+++ src/battleship/Ships.java
@@ -1,7 +1,36 @@
 
 package battleship;
-
-
+import java.util.ArrayList;
+import java.awt.*;
 public class Ships {
+private static ArrayList<Ships> ships = new ArrayList<Ships>(); 
+private Type type;
+private boolean show;
+////////////////////////////////////////////////////////////////////////////////
+public static enum Type {
+    ONE_ONE,TWO_ONE,THREE_ONE
+};
+////////////////////////////////////////////////////////////////////////////////
+public static Ships create(Type type)
+{
+Ships ptr = new Ships(type);
+ships.add(ptr);
+return (ptr);    
+}         
+Ships(Type _type)
+{
+type=_type;    
+}
+public void draw()
+{
+if(show)
+{
     
+}
+else
+{    
+return;    
+}   
+}
+
 }
