cf00c4aa3f586faeba7f020303e5b5932fbc67f0
Alexis LaparraPerez-150003278 <150003278@vusdnoc.vusd.k12.ca.us>
2018-11-08 09:51:22 -0800

    ALEXIS

--- src/battleship/Ships.java
+++ src/battleship/Ships.java
@@ -5,7 +5,9 @@ import java.awt.*;
 public class Ships {
 private static ArrayList<Ships> ships = new ArrayList<Ships>(); 
 private Type type;
-private boolean show;
+private static boolean show=true;
+private boolean hit;
+private int health;
 ////////////////////////////////////////////////////////////////////////////////
 public static enum Type {
     ONE_ONE,TWO_ONE,THREE_ONE
@@ -20,18 +22,39 @@ return (ptr);
 Ships(Type _type)
 {
 type=_type;    
-show=true;
+hit=false;
+setHealth();
 }
+////////////////////////////////////////////////////////////////////////////////
 public void draw(Graphics2D g)
 {
 if(show)
 {
-    
+//g.fillOval(x, y, xDelta*health, ydelta);
 }
+
 else
 {    
 return;    
-}   
-}
+} 
 
+}
+////////////////////////////////////////////////////////////////////////////////
+public void setHealth()
+{
+if(type==Ships.Type.ONE_ONE)
+health=1;
+else if(type==Ships.Type.TWO_ONE)
+health=2;
+else if(type==Ships.Type.THREE_ONE)
+health=3;
+
+}        
+
+
+public void setShow(boolean is)
+{
+show=is;
+}   
+////////////////////////////////////////////////////////////////////////////////
 }
